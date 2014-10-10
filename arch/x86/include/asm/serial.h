c0eaa3bb944a45ae32c12d69551287aec694245e
Ong Boon Leong <boon.leong.ong@intel.com>
2014-10-10 08:24:00 +0800

    TBD: serial: serial console hack
    This is temporary hack to make Quark serial console works.
    It is inherited from Bryan O'Donoghue.
    
    Signed-off-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

--- arch/x86/include/asm/serial.h
+++ arch/x86/include/asm/serial.h
@@ -8,7 +8,11 @@
  * clock, since the 16550A is capable of handling a top speed of 1.5
  * megabits/second; but this requires the faster clock.
  */
+#if 0
 #define BASE_BAUD ( 1843200 / 16 )
+#else
+#define BASE_BAUD 2764800
+#endif
 
 /* Standard COM flags (except for COM4, because of the 8514 problem) */
 #ifdef CONFIG_SERIAL_DETECT_IRQ
@@ -19,11 +23,15 @@
 #define STD_COM4_FLAGS ASYNC_BOOT_AUTOCONF
 #endif
 
+#if 0
 #define SERIAL_PORT_DFNS			\
 	/* UART CLK   PORT IRQ     FLAGS        */			\
 	{ 0, BASE_BAUD, 0x3F8, 4, STD_COM_FLAGS },	/* ttyS0 */	\
 	{ 0, BASE_BAUD, 0x2F8, 3, STD_COM_FLAGS },	/* ttyS1 */	\
 	{ 0, BASE_BAUD, 0x3E8, 4, STD_COM_FLAGS },	/* ttyS2 */	\
 	{ 0, BASE_BAUD, 0x2E8, 3, STD_COM4_FLAGS },	/* ttyS3 */
+#else
+#define SERIAL_PORT_DFNS
+#endif
 
 #endif /* _ASM_X86_SERIAL_H */
