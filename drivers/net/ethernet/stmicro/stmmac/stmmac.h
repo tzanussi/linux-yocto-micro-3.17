192535963cf654252cd5628243412dcb77fffcfd
Tom Zanussi <tom.zanussi@linux.intel.com>
2014-10-22 11:51:36 -0500

    stmmac: Make ethtool support optional
    If NET_ETHTOOL is turned off, we don't need any ethtool support in the
    driver.
    
    Signed-off-by: Tom Zanussi <tom.zanussi@linux.intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac.h
@@ -121,7 +121,13 @@ struct stmmac_priv {
 int stmmac_mdio_unregister(struct net_device *ndev);
 int stmmac_mdio_register(struct net_device *ndev);
 int stmmac_mdio_reset(struct mii_bus *mii);
+
+#ifdef CONFIG_NET_ETHTOOL
 void stmmac_set_ethtool_ops(struct net_device *netdev);
+#else
+static inline void stmmac_set_ethtool_ops(struct net_device *netdev) {}
+#endif
+
 extern const struct stmmac_desc_ops enh_desc_ops;
 extern const struct stmmac_desc_ops ndesc_ops;
 extern const struct stmmac_hwtimestamp stmmac_ptp;
